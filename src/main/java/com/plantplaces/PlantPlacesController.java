ef911fcb2e313ece61b33aaf2ea0057262f31754
csros <csros@MAXWELL.zoomtown.com>
2020-09-17 14:38:58 -0400

    update / endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -12,15 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 	
+	/**
+	 *Handle the /start endpoint
+	 * @return
+	 */
 	@RequestMapping("/start")
 	public String Start() {
 		
 		return "start";
 	}
 	
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String index() {
 		
 		return "start";
 	}
+	
 }
